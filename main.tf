13cee03e908ffdd44f05dfeae625b23eac518b3c
Sridhar S K <56919037+sridhar7601@users.noreply.github.com>
2025-02-25 11:30:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
